4df02ab529cbd59f0111c319f6dd2ae03b66586a
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-06 15:57:09 +0530

    create rg with terraform

--- terraform-resource-group/main.tf
+++ terraform-resource-group/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=1.0.0"
   required_providers {
     azurerm = {
-        soursource = "heshicorp/azurerm"
+        source = "heshicorp/azurerm"
         version = ">=2.0"
     }
   }

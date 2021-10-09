42414964b85f69d332314f6c48f86a3758d08f62
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-09 21:33:03 +0530

    Use of meta argument to add explicit dependency

---
+++ Meta-Argument-depends_on/Virtual-Network.tf
@@ -0,0 +1,49 @@
+# Resource-2: Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-1"
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  tags = {
+    "Name" = "vnet-1"
+  }
+}
+
+# Resource-3: Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Resource-4: Create Public IP Address
+resource "azurerm_public_ip" "publicip" {
+  # Add Explicit Dependency to have this resource created only after Virtual Network and Subnet Resources are created. 
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    azurerm_subnet.subnet
+  ]
+  name                = "publicip-1"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  allocation_method   = "Static"
+  tags = {
+    environment = "Dev"
+  }
+}
+
+# Resource-5: Create Network Interface
+resource "azurerm_network_interface" "vmnic" {
+  name                = "vmnic"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
